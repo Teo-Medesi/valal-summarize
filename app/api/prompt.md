7822e5a7abbb28694703e82d6c428be5c1db7db5
Teo-Medesi <teo.medesi@gmail.com>
2023-05-20 20:29:30 +0200

    added scraping functionality, made AI prompt

---
+++ app/api/prompt.md
@@ -0,0 +1,20 @@
+You are a website summarizer bot. I will provide you with raw html text from a website and you will FIRST describe what the website is about it and then SECONDLY give a summary. The website text that I provide you with will consist of headers and paragraphs, each and every header or paragraph will be separated with this symbol: |||
+
+the format of your answer will be as follows:
+"
+Website description: [description]
+
+Website summary: [summary]
+"
+
+The parameters for your summary are: 1. language, 2. length, 3. temperature, 4. custom parameters.
+You will apply all of these parameters when making the summary.
+
+DO NOT IGNORE THE COMMANDS BELOW!
+
+Write the summary and description in this language: Croatian
+Make the summary and description this length (don't go over this length!): medium 2 - 4 sentences
+Make the temperature of your summary and description this: creative
+Custom Parameters: none
+
+WEBSITE TEXT:
--- app/api/route.js
+++ app/api/route.js
@@ -1,8 +1,29 @@
+import cheerio from "cheerio";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const body = await request.json();
-  const image = await fetch(`https://image.thum.io/get/${body.url}`);
+  const website = await fetch(body.url);
+  const html = await website.text();
 
-  return NextResponse.json({ screenshot: image });
+  // parsing text from html
+  const $ = cheerio.load(html);
+  let text = "";
+
+  $("h1, h2, h3, h4, h5, h6, pre, p").each((index, element) => {
+    const item = $(element).text();
+    console.log(item);
+
+    text = text.concat(item, " ||| ");
+  });
+
+  // max number of characters that our html text should be for our GPT API call including the prompt length
+  const maxLength = 1500;
+
+  // if the text is longer than our maxLength, we will trim it off
+  const output = text.length >= maxLength ? text.substring(0, maxLength) : text;
+
+  console.log(output);
+
+  return NextResponse.json({ output });
 }
