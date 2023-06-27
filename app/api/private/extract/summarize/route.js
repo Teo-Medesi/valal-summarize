de40c7c0532c6385f3b68e53ba6dfec1b527c0d6
Teo-Medesi <teo.medesi@gmail.com>
2023-06-27 14:49:35 +0200

    rate limiting implemented

--- app/api/private/extract/summarize/route.js
+++ app/api/private/extract/summarize/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import summarize from "./completion";
 import extract from "../extract";
 import extractVariables from "./extract";
+import updateMetadata from "./updateMetadata";
 
 // body => options, url => language, temperature, length, custom
 
@@ -11,11 +12,16 @@ export async function POST(request) {
     const options = body.options || { language: "English", length: "Medium - 2-4 sentences", temperature: 0.5, custom: "none" }
 
     // getting headers and paragraphs from website
-    const text = await extract(body.url);
-    const summary = await summarize(text, options);
-    const { websiteDescription, websiteSummary } = extractVariables(summary);
+    //const text = await extract(body.url);
+    //const summary = await summarize(text, options);
+    //const { websiteDescription, websiteSummary } = extractVariables(summary);
 
-    return NextResponse.json({ website_description: websiteDescription, website_summary: websiteSummary });
+    // updating rate limit
+    const auth = JSON.parse(request.headers.get("Authorization"));
+    await updateMetadata(auth.user_id);
+
+    return NextResponse.json({ message: "test" });
+    // return NextResponse.json({ website_description: websiteDescription, website_summary: websiteSummary });
   }
   catch (error) {
     return NextResponse.json({ error }, { status: 500 });
---
+++ app/api/private/extract/summarize/updateMetadata.js
@@ -0,0 +1,20 @@
+async function updateMetadata(userID) {
+  // we want to send a PATCH request to update or create a new rate_limit object which will include:
+  // rate_limit, requests_made
+
+  const response = await fetch(`http://localhost:3000/api/public/users/${userID}/metadata`)
+  const data = await response.json();
+  const metadata = data.app_metadata;
+
+
+  const new_metadata = (metadata.rate_limit && metadata.requests_made) ? { requests_made: metadata.requests_made + 1 }
+    : { requests_made: 1, rate_limit: 100 };
+
+  await fetch(`http://localhost:3000/api/public/users/${userID}/metadata`, {
+    method: "PATCH",
+    body: JSON.stringify({ new_metadata })
+  })
+
+}
+
+export default updateMetadata;
