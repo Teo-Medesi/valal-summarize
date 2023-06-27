d30896100c3cebeb2ff60e3763b89596b36f1212
Teo-Medesi <teo.medesi@gmail.com>
2023-06-27 17:07:16 +0200

    undid some debug code

--- app/api/private/extract/summarize/route.js
+++ app/api/private/extract/summarize/route.js
@@ -12,16 +12,15 @@ export async function POST(request) {
     const options = body.options || { language: "English", length: "Medium - 2-4 sentences", temperature: 0.5, custom: "none" }
 
     // getting headers and paragraphs from website
-    //const text = await extract(body.url);
-    //const summary = await summarize(text, options);
-    //const { websiteDescription, websiteSummary } = extractVariables(summary);
+    const text = await extract(body.url);
+    const summary = await summarize(text, options);
+    const { websiteDescription, websiteSummary } = extractVariables(summary);
 
     // updating rate limit
     const auth = JSON.parse(request.headers.get("Authorization"));
     await updateMetadata(auth.user_id);
 
-    return NextResponse.json({ message: "test" });
-    // return NextResponse.json({ website_description: websiteDescription, website_summary: websiteSummary });
+    return NextResponse.json({ website_description: websiteDescription, website_summary: websiteSummary });
   }
   catch (error) {
     return NextResponse.json({ error }, { status: 500 });
